b7cda1a850fa1b08ba60ea869d42a49be79ce9a5
vinisi <vini22004@gmail.com>
2015-05-10 16:09:08 -0300

    g

--- cliente.py
+++ cliente.py
@@ -1,29 +1,30 @@
-import socket
-import sys
-import time
-
-HOST	 = '10.0.0.1' 	# Endereco IP do Servidor
-PORT = sys.argv[1]	# Porta que o Servidor esta
-udp = socket.socket(socket.AF_INET, #internet
-	socket.SOCK_DGRAM) #UDP
-dest = (HOST, PORT)
-udp.connect(dest)
-cont = 1
-num_msg = 5000
-tam_msg = bytearray(1000)
-
-#informa ao servidor quantas msg serao enviadas
-udp.send (num_msg)
-
-inicio = time.time()
-while cont <= num_msg:
-	if udp.send (tam_msg):
-		print 'Enviando msg '+ str(cont) + 'de ' + str(sys.getsizeof(bytearray(1000))) + 'B'
-		cont = cont + 1
-
-print 'Recebendo msg de ' + str(sys.getsizeof(bytearray(udp.recv(1)))) + 'B'
-fim = time.time() - inicio
-print 'Tempo total de execucao' + str (fim)
-udp.close()
-print 'Encerrando conexao com o servidor'
-sys.exit(0)
+import socket   #for sockets
+import sys  #for exit
+ 
+# create dgram udp socket
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+except socket.error:
+    print 'Failed to create socket'
+    sys.exit()
+ 
+host = '10.0.0.1';
+port = int(sys.argv[1]);
+ 
+while(1) :
+    msg = raw_input('Enter message to send : ')
+     
+    try :
+        #Set the whole string
+        s.sendto(msg, (host, port))
+         
+        # receive data from client (data, addr)
+        d = s.recvfrom(1024)
+        reply = d[0]
+        addr = d[1]
+         
+        print 'Server reply : ' + reply
+     
+    except socket.error, msg:
+        print 'Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+        sys.exit()
